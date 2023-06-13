9ead4a626ec0dbb1924085f63503d605077cf9db
João Felipe <joaofeliperex64@gmail.com>
2023-06-12 20:06:03 -0400

    add route clientes

--- src/api/components/index.ts
+++ src/api/components/index.ts
@@ -21,6 +21,8 @@ export function registerRoutes(router: Router, prefix: string = ''): void {
   router.use(`${prefix}/deposito`, new DepositoRoutes().routes())
   router.use(`${prefix}/saque`, new AgenciaRoutes().routes())
   router.use(`${prefix}/transferencia`, new AgenciaRoutes().routes())
+  router.use(`${prefix}/clientes`, new AgenciaRoutes().routes())
+
 
 
 
